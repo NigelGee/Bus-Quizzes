8b10b4497e4e0de7f66ab5343ca95ec5b8f80bd9
Nigel Gee <nigel.gee@live.com>
2018-08-23 19:59:34 +0100

    Commit UI Update func

--- Bus Quizzes/Controllers/ViewController.swift
+++ Bus Quizzes/Controllers/ViewController.swift
@@ -11,26 +11,77 @@ import UIKit
 class ViewController: UIViewController {
     
     let allQuestion = QuestionBank()
+    let allAnswer = AnswerBank()
+    var pickedAnswer : Bool = false
+    var usedQuestion = [Int]()
 
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var imageQuestion: UIImageView!
     @IBOutlet weak var progressLabel: UIView!
+    @IBOutlet weak var answerText1: UIButton!
+    @IBOutlet weak var answerText2: UIButton!
+    @IBOutlet weak var answerText3: UIButton!
+    @IBOutlet weak var answerText4: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestion.questionList[1]
-        questionLabel.text = firstQuestion.questionText
-        imageQuestion.image = UIImage(named: firstQuestion.questionImage)
+        let maxQuestion : Int = allQuestion.questionList.count
+        let maxAnswer : Int = allAnswer.answerList.count
+        
+        if maxAnswer != maxQuestion * 4 {
+            fatalError("\(maxQuestion) question should have \(maxQuestion * 4) answers with only have \(maxAnswer) answers")
+        }
+        
+        checkRandomQuestion(maximumQuestion: maxQuestion)
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillLayoutSubviews() {
+        questionLabel.sizeToFit()
     }
 
+    
+    //MARK:- Update to UI views
+    func updateUI (randomNumber number : Int) {
+        
+        let question = allQuestion.questionList[number]
+        
+        questionLabel.text = question.questionText
+        
+        if question.questionImage != "" {
+            imageQuestion.image = UIImage(named: question.questionImage)
+        }
+        
+        let answerNumber = number * 4
+        let answerA = allAnswer.answerList[answerNumber]
+        let answerB = allAnswer.answerList[answerNumber + 1]
+        let answerC = allAnswer.answerList[answerNumber + 2]
+        let answerD = allAnswer.answerList[answerNumber + 3]
+        
+        answerText1.setTitle(answerA.answerText, for: .normal)
+        answerText2.setTitle(answerB.answerText, for: .normal)
+        answerText3.setTitle(answerC.answerText, for: .normal)
+        answerText4.setTitle(answerD.answerText, for: .normal)
+        
+    }
+    
+    //MARK- Pick Random Questions from Array
+    func randomQuestion () {
+        
+    }
+    
+    func checkRandomQuestion (maximumQuestion: Int) {
+        
+        let randomNumber = Int(arc4random_uniform(UInt32(maximumQuestion)))
+        
+        updateUI(randomNumber: randomNumber)
+        
+    }
+    
     @IBAction func answerPressed(_ sender: UIButton) {
+        
     }
     
 }
--- Bus Quizzes/Model/AnswerBank.swift
+++ Bus Quizzes/Model/AnswerBank.swift
@@ -16,14 +16,20 @@ class AnswerBank {
         
         //MARK:- Q1
         answerList.append(Answer(text: "Low flying Motorbikes", correctAnswer: false))
-        answerList.append(Answer(text: "Motor Vecheles prohibted", correctAnswer: true))
+        answerList.append(Answer(text: "Motor Vecheles prohibited", correctAnswer: true))
         answerList.append(Answer(text: "Car below, Motorbikes above", correctAnswer: false))
-        answerList.append(Answer(text: "Cars and Motorbikes prohibeted", correctAnswer: false))
+        answerList.append(Answer(text: "Cars and Motorbikes prohibited", correctAnswer: false))
         
         //MARK:- Q2
         answerList.append(Answer(text: "10 Meters", correctAnswer: false))
         answerList.append(Answer(text: "15 Meters", correctAnswer: true))
         answerList.append(Answer(text: "20 Meters", correctAnswer: false))
         answerList.append(Answer(text: "25 Meters", correctAnswer: false))
+        
+        //MARK:- Q3
+        answerList.append(Answer(text: "Latin", correctAnswer: true))
+        answerList.append(Answer(text: "French", correctAnswer: false))
+        answerList.append(Answer(text: "Thai", correctAnswer: false))
+        answerList.append(Answer(text: "Spanish", correctAnswer: false))
     }
 }
